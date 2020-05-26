8989cc364e5f95ebf4a306e0b02d03298f15e7cd
William Oliveira <sudowilliam@gmail.com>
2020-05-26 03:55:07 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+import * as THREE from 'three';
+import hulkImage from '../images/hulk.jpg';
+
+const canvas = document.getElementById('canvas');
+
+let camera;
+let scene;
+let renderer;
+
+let geometry;
+let material;
+let texture;
+let mesh;
+
+window.onload = () => {
+  init();
+  animate();
+};
+
+const init = () => {
+  camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  camera.position.z = 1;
+
+  scene = new THREE.Scene();
+
+  texture = new THREE.TextureLoader().load(hulkImage);
+  geometry = new THREE.BoxGeometry(0.3, 0.5, 0.04);
+  material = [
+    new THREE.MeshBasicMaterial({ color: '#282b3f' }),
+    new THREE.MeshBasicMaterial({ color: '#282b3f' }),
+    new THREE.MeshBasicMaterial({ color: '#282b3f' }),
+    new THREE.MeshBasicMaterial({ color: '#282b3f' }),
+    new THREE.MeshBasicMaterial({ map: texture }),
+    new THREE.MeshBasicMaterial({ map: texture }),
+  ];
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
+  renderer.setClearColor(new THREE.Color('#1e202f'));
+};
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  mesh.rotation.x += 0.02;
+  mesh.rotation.y += 0.02;
+
+  renderer.render(scene, camera);
+};
